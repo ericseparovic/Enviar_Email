cecbf30ab73e9b7ed548745ecdae748daa087528
Eric Separovic <ericseparovic07@gmail.com>
2022-07-12 22:12:46 -0300

    Validation of the form is created and a spinner is shown to simulate sending the email

--- js/app.js
+++ js/app.js
@@ -1,42 +1,98 @@
 // HTML element selection
-const email = document.querySelector('#email')
-const asunto = document.querySelector('#asunto')
-const mensaje = document.querySelector('#mensaje')
-
+const email = document.querySelector("#email");
+const asunto = document.querySelector("#asunto");
+const mensaje = document.querySelector("#mensaje");
+const btnSubmit = document.querySelector("#enviar");
+const spinner = document.querySelector("#spinner");
 
 // Events
-email.addEventListener('blur', getData)
-asunto.addEventListener('blur', getData)
-mensaje.addEventListener('blur', getData)
+email.addEventListener("keyup", getData);
+asunto.addEventListener("keyup", getData);
+mensaje.addEventListener("keyup", getData);
+btnSubmit.addEventListener("click", sendData);
 
-//Get data from HTML 
+const data = {
+  email: "",
+  asunto: "",
+  mensaje: "",
+};
+
+//Get data from HTML
 function getData(e) {
-    
-    const data = {
-        email: "",
-        asunto: "",
-        mensaje: ""
-    }
-    
-    data.email = email.value
-    data.asunto = asunto.value
-    data.mensaje = mensaje.value
+  if (e.target.id == "email") {
+    data.email = e.target.value;
+    validationEmail();
+  }
 
-    validation(data)
+  if (e.target.id == "asunto") {
+    console.log("validation asunto");
+    data.asunto = e.target.value;
+    validationAsunto();
+  }
 
+  if (e.target.id == "mensaje") {
+    data.mensaje = e.target.value;
+    validationMensaje();
+  }
+
+  // Cuando los campos del formulario esta completo se remueve la clase de opacidad y de no permitido
+  activeBtnSubmit();
 }
 
+function sendData(e) {
+  e.preventDefault();
+  if (validationEmail() && validationAsunto() && validationMensaje()) {
+    spinner.style.display = "flex";
+    setTimeout(() => {
+      spinner.style.display = "none";
+    }, 1500);
 
-function validation(data){
-    const er = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
-
-    const {email, asunto, mensaje} = data
-
-    if(er.test(email)){
-        console.log('correo valido')
-    } else {
-        console.log('invalido')
-    }
+    return console.log("Enviar formulario");
+  }
 }
 
+function validationMensaje() {
+  if (mensaje.value) {
+    mensaje.classList.remove("border-red-500");
+    mensaje.classList.add("border-green-500");
+    return true;
+  } else {
+    mensaje.classList.remove("border-green-500");
+    mensaje.classList.add("border-red-500");
+    return false;
+  }
+}
 
+function validationAsunto() {
+  if (asunto.value) {
+    asunto.classList.remove("border-red-500");
+    asunto.classList.add("border-green-500");
+    return true;
+  } else {
+    asunto.classList.remove("border-green-500");
+    asunto.classList.add("border-red-500");
+    return false;
+  }
+}
+
+function validationEmail() {
+  const er = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+  if (er.test(email.value)) {
+    email.classList.remove("border-red-500");
+    email.classList.add("border-green-500");
+    return true;
+  } else {
+    email.classList.remove("border-green-500");
+    email.classList.add("border-red-500");
+    return false;
+  }
+}
+
+function activeBtnSubmit() {
+  if (email.value && asunto.value && mensaje.value) {
+    btnSubmit.classList.remove("cursor-not-allowed", "opacity-50");
+  } else {
+    btnSubmit.classList.add("cursor-not-allowed", "opacity-50");
+  }
+}
